c1be32266ccdf425ed57f4e549abca39bd9d664c
sondos-ui <76984205+sondos-ui@users.noreply.github.com>
2021-01-13 12:53:12 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+var btns = document.querySelectorAll(".drum");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var btnInnerHtml=this.innerHTML;
+    checkLetter(btnInnerHtml);
+  });
+}
+document.addEventListener("keydown",function(event){
+  var keyPressed=event.key;
+  checkLetter(keyPressed);
+});
+function checkLetter(letter)
+{
+  switch (letter) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      var kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+    case "l":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    default:
+  }
+}
